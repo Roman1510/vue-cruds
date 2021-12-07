377eb589c4b9fb26cadf487b658e210d9bacef3a
Roman1510 <romanvinnick@gmail.com>
2021-12-07 14:12:38 +0100

    added bulma, and configured lint

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/"> FAQ </a>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  </nav>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -14,5 +14,11 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
 };
 </script>
