884fe7798df6b16231d8b80887131c0cccbd450a
Roman1510 <romanvinnick@gmail.com>
2021-12-08 19:28:29 +0100

    added just one task for refactoring

--- src/main.js
+++ src/main.js
@@ -8,3 +8,4 @@ createApp(App).use(store).use(router).mount('#app');
 // add delete button, with the delete functionality
 // add edit button (pencil), with the edit functionality
 // add delete all button, with the modal window and delete all loop
+// refactor current logic, so the api calls are separated, and used generically 
